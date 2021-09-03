9ae654b7dc237fd82e8a8f53c9756b4ac59f1e18
dehb <teejaysunimi@gmail.com>
2021-09-03 10:28:29 +0100

    Converteed details activity to word list fragment

--- app/src/main/java/com/example/wordsapp/WordListFragment.kt
+++ app/src/main/java/com/example/wordsapp/WordListFragment.kt
@@ -5,11 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.wordsapp.databinding.FragmentWordListBinding
 
 
 class WordListFragment : Fragment() {
+
+
+    companion object {
+        const val LETTER = "letter"
+        const val SEARCH_PREFIX = "https://www.google.com/search?q="
+    }
 
     private var _binding: FragmentWordListBinding? = null
     private val binding get() = _binding!!
@@ -31,6 +39,25 @@ class WordListFragment : Fragment() {
     }
 
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        recyclerView = binding.recyclerView
 
+        val letterId = activity?.intent?.extras?.getString("LETTER").toString()
+
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = WordAdapter(letterId, requireContext())
+
+        // Adds a [DividerItemDecoration] between items
+        recyclerView.addItemDecoration(
+            DividerItemDecoration(context, DividerItemDecoration.VERTICAL)
+        )
+
+//        title = getString(R.string.detail_prefix) + " " + letterId
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 
 }
